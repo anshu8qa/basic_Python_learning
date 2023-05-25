6103d11ae2299012c6f313a043fe8e5e5b2cd044
Anshu <anshu@SatishKs-MBP.hsd1.ca.comcast.net>
2023-05-24 22:07:40 -0700

    Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hi all, I am Anshu Puspam")
+print("Hi all, I am")
 print("I very good QA tester")
-print('STUDENT NAME : Anshu Puspam \nE-MAIL : anshu.pushpam@gmail.com \nMAJOR:Computer science')
+print('STUDENT NAME : Anshu8qa\nE-MAIL : ansh@l.com \nMAJOR:Computer science')
 print('COMPUTER PROGRAMMING EXPERIENCE : None')
 print('The computer can compute: 12345679 * 9')
 print('and the product is:', 12345679 * 9)
