29353c599071a796fcd7252cd8ba65704a90157b
anshu8qa <anshu.pushpam@gmail.com>
2023-10-06 22:25:59 -0700

    second commit

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ print('The computer can compute: 12345679 * 9')
 print('and the product is:', 12345679 * 9)
 print('delete branch')
 
+birth_year = 1988
+current_year = 2023
+age = current_year - birth_year
+print(age)
 
+
+print(3 >= 3)
+
+
+
+""" Date - 10-06-2023 code """
+print(7 / 4)
+print(7 // 4)
+print(7 % 4)
 
